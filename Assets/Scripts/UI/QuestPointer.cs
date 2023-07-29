4057fe4916806520cb299a5c2141de2067f8b418
Khairil Azmyr <azmyr36@gmail.com>
2023-07-29 23:14:39 +0800

    animations and UI

--- Assets/Scripts/UI/QuestPointer.cs
+++ Assets/Scripts/UI/QuestPointer.cs
@@ -8,31 +8,54 @@ public class QuestPointer : MonoBehaviour
     public Transform target;
     public Image image;
 
+    [Header("Image Scaling")]
+    public float minDistance = 2f;
+    public float maxDistance = 5f;
+    public float minScale = 0.5f;
+    public float maxScale = 1f;
+
     private void Update()
     {
+        if (target == null)
+        {
+            image.enabled = false;
+            return;
+        }
+
+        image.enabled = true;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(target.position);
+
         float minX = image.GetPixelAdjustedRect().width / 4;
         float maxX = Screen.width - minX;
 
         float minY = image.GetPixelAdjustedRect().height / 4;
         float maxY = Screen.height - minY;
 
-        Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
-
         if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
         {
-            if (pos.x < Screen.width / 2)
+            if (screenPos.x < Screen.width / 2)
             {
-                pos.x = maxX;
+                screenPos.x = maxX;
             }
             else
             {
-                pos.x = minX;
+                screenPos.x = minX;
             }
         }
 
-        pos.x = Mathf.Clamp(pos.x, minX, maxX);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        screenPos.x = Mathf.Clamp(screenPos.x, minX, maxX);
+        screenPos.y = Mathf.Clamp(screenPos.y, minY, maxY);
 
-        image.transform.position = pos;
+        image.transform.position = screenPos;
+
+        Vector3 directionToTarget = target.position - Camera.main.transform.position;
+        float angleToTarget = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+        image.transform.rotation = Quaternion.Euler(0f, 0f, angleToTarget - 180f);
+
+        float distanceToTarget = Vector3.Distance(target.position, Camera.main.transform.position);
+        float normalizedDistance = Mathf.Clamp01((distanceToTarget - minDistance) / (maxDistance - minDistance));
+        float scale = Mathf.Lerp(maxScale, minScale, normalizedDistance); 
+        image.transform.localScale = new Vector3(scale, scale, 1f);
     }
 }
